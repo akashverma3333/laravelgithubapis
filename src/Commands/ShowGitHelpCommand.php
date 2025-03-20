4bf0fb3f576d1d0678275c50bd2811892076a231
Akash Verma <akashverma75890@gmail.com>
2025-03-20 13:46:24 +0530

    Update ShowGitHelpCommand.php

--- src/Commands/ShowGitHelpCommand.php
+++ src/Commands/ShowGitHelpCommand.php
@@ -36,9 +36,16 @@ Saved GitHub Commands:
 7. Save all Git commands for reference:
    php artisan showallcommands
 
-8. Branch Checkout
+8. Branch Checkout:
    php artisan branch:checkout <user id>/<repo> <branch>
    example:php artisan branch:checkout akashverma3333/laravelgithubapis main
+            
+9. Create new branch:
+   php artisan create-branch <user id>/<repo> <new_branch> <base_branch>
+   example:php artisan create-branch akashverma3333/laravelgithubapis new-branch main
+
+10. Create a Pull Request:
+    php artisan create-pr
 EOT;
 
         $this->info($helpText);
